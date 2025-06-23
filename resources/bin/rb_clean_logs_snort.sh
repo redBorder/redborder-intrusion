c28f0fef8944b2f3aa29ed0c166aaac57dce3be7
david vhk <34706472+davidredborder@users.noreply.github.com>
2025-06-23 15:27:15 +0100

    release 0.3.3 (#21)
    * Feature/apply update job intrusion (#16)
    * Apply update job snort3
    * Update rb_verify_snort
    * modify get_sensor_rules.sh
    * Add rb_remap_intrusion_rules and call in get_sensor_rules
    * Add init.d status check
    * Modify rb_check_snort.rb
    * move snort3 to rc.d
    * add cdomain to get rules from webui
    
    ---------
    
    Co-authored-by: Miguel Álvarez <128592227+malvads@users.noreply.github.com>
    Co-authored-by: Rafa Gómez <rgomez@redborder.com>
    Co-authored-by: Juan Soto <127120525+jsotofernandez@users.noreply.github.com>

--- resources/bin/rb_clean_logs_snort.sh
+++ resources/bin/rb_clean_logs_snort.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-for instance in $(ls -d /var/log/snort/instance-* 2>/dev/null); do
-    pushd ${instance}/archive &>/dev/null
-    find . -name "snort.log.*" -mtime +30 -delete
-    popd &>/dev/null
+BASE_DIR="/etc/snort"
+
+find "$BASE_DIR" -maxdepth 1 -mindepth 1 -type d | while read -r instance_dir; do
+  dir_name=$(basename "$instance_dir")
+
+  if [[ "$(grep -o '_' <<< "$dir_name" | wc -l)" -eq 2 ]]; then
+    RAW_DIR="$instance_dir/raw"
+
+    if [ -d "$RAW_DIR" ]; then
+      echo "Cleaning $RAW_DIR ..."
+
+      find "$RAW_DIR" -maxdepth 1 -mindepth 1 -type d -mtime +30 -exec rm -rf {} \;
+    fi
+  fi
 done
