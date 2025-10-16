12056215b8d71c94af512a870d24a9744e9a96c4
ljblancoredborder <ljblanco@redborder.com>
2025-10-16 13:45:29 +0100

    also we declare endpoint in rules_cloud. Changed

--- resources/scripts/rb_get_sensor_rules_cloud.rb
+++ resources/scripts/rb_get_sensor_rules_cloud.rb
@@ -165,7 +165,7 @@ end
 
 ret=0
 cdomain = File.read('/etc/redborder/cdomain').strip rescue 'redborder.cluster'
-@weburl = "webui.#{cdomain}"
+@weburl = "webui.service.#{cdomain}"
 
 def usage
   printf "Usage: rb_get_sensor_rules_cloud.rb -u <uuid> -c <command>\n"
